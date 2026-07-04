26e611324d57e12e2e89a987d47c8aff180a11e9
Manideep Bishnoi <manideepbishnoi145@gmail.com>
2026-07-04 11:55:49 +0530

    Change AWS region and AMI in main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-051bfa33df3949860"
   instance_type = "t2.micro"
 
   tags = {
